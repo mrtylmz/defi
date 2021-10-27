6d26251d892f708588e90fa7acdd31c403f2c84c
MURAT YILMAZ <muyilmaz@gmail.com>
2021-10-27 17:22:44 -0500

    Update EthPrice.sol
    price comes from avalanche feeds on chainlink oracle

--- EthPrice.sol
+++ EthPrice.sol
@@ -7,7 +7,7 @@ contract EthPrice {
   AggregatorV3Interface internal priceFeed;
 
   constructor() {
-    // ETH / USD
+    // ETH / USD // https://docs.chain.link/docs/avalanche-price-feeds/
     priceFeed = AggregatorV3Interface(0x86d67c3D38D2bCeE722E601025C25a575021c6EA);
   }
 
